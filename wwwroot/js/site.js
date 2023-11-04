a4b4aa8d888fbc762d75480bc7bab472f6aba87a
ErvinW <116048866+ErvinW@users.noreply.github.com>
2023-11-05 00:08:17 +0800

    Keybinds

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+document.addEventListener("keydown", e => {
+    if (e.key.toLowerCase() === "t") {
+        window.location.href = 'User/Transfer';
+    }
+});
+
