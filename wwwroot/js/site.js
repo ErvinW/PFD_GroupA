e688ef5dfb03cb1e186f6e62723a76355fbd1778
Xin Yin <xinyin322@gmail.com>
2023-11-05 14:32:38 +0800

    Speech to text first draft

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,12 +1,40 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
 
+
+
+// Keybinds
 
 document.addEventListener("keydown", e => {
     if (e.key.toLowerCase() === "t") {
         window.location.href = 'User/Transfer';
     }
 });
+
+
+// Speech to text recognition
+
+const output = document.getElementById('output');
+const recognition = new webkitSpeechRecognition();
+
+recognition.continuous = true;
+recognition.interimResults = false;
+recognition.lang = 'en-US'; 
+
+recognition.onresult = function (event) {
+    const result = event.results[event.results.length - 1][0].transcript;
+    console.log(result)
+
+
+    if (result.toLowerCase().includes('go to transfer')) {
+        window.location.href = 'User/Transfer';
+    }
+};
+
+recognition.start(); 
+
+
+
+
 
