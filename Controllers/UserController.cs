4bff31f3d15c65f72e282a25f9a43f0ccdb8bafc
NgBiDe <S10239969@connect.np.edu.sg>
2023-11-15 16:30:37 +0800

    update python script

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -20,16 +20,26 @@ namespace PFD_GroupA.Controllers
 
         public void RunPythonScript()
         {
-            ScriptEngine engine = Python.CreateEngine();
-            ICollection<string> Paths = engine.GetSearchPaths();
-            Paths.Add("\"D:\\YEAR 2 SEM 2\\PFD\\Solution\\Python Script\\myenv\\Lib\\site-packages\\cv2\"");
-            Paths.Add("\"D:\\YEAR 2 SEM 2\\PFD\\Solution\\Python Script\\myenv\\Lib\\site-packages\\mediapipe\"");
-            Paths.Add("\"D:\\YEAR 2 SEM 2\\PFD\\Solution\\Python Script\\myenv\\Lib\\site-packages\\pyautogui\"");
-            engine.SetSearchPaths(Paths);
+            var engine = Python.CreateEngine();
+            var searchPaths = engine.GetSearchPaths();
 
+            Console.WriteLine("IronPython Search Paths:");
+            foreach(var path in searchPaths)
+            {
+                Console.WriteLine(path + " lolol");
+            }
+
+            ICollection<string> paths = engine.GetSearchPaths();
+            paths.Add(".");
+            paths.Add("D:\\YEAR 2 SEM 2\\PFD\\Solution\\bin\\Debug\\net6.0\\lib");
+            paths.Add("D:\\YEAR 2 SEM 2\\PFD\\Solution\\bin\\Debug\\net6.0\\DLLs");
+            engine.SetSearchPaths(paths);
             // Load the Python script
             var scriptPath = "D:\\YEAR 2 SEM 2\\PFD\\Solution\\Python Script\\pythontest.py"; // Replace this with your script's path
-            engine.ExecuteFile(scriptPath);
+            var scope = engine.CreateScope();
+            var source = engine.CreateScriptSourceFromFile(scriptPath);
+            source.Execute(scope);
+            //engine.ExecuteFile(scriptPath);
         }
 
 		// GET: UserController
@@ -39,7 +49,8 @@ namespace PFD_GroupA.Controllers
             var UID = JsonSerializer.Deserialize<User>(ID);
             string UserID = UID.UserID;
 
-            } */
+            //RunPythonScript(); 
+
             return View();
         }
 
@@ -50,10 +61,10 @@ namespace PFD_GroupA.Controllers
 			List<Transactions> OutgoingTransactions = transactionsContext.GetSenderTransactions(AccountObject.UserID);
             */
             return View();
-        }
+        
 
-			UserKeybinds keybinds = keybindContext.GetUserKeybinds(UserID);
-			return View(keybinds);
+			//UserKeybinds keybinds = keybindContext.GetUserKeybinds(UserID);
+			//return View(keybinds);
         }
 
         [HttpGet]
