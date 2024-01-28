12c6c2a3be1b527044409b029458a846643e3a5a
Xin Yin <s10243745@connect.np.edu.sg>
2024-01-28 23:57:34 +0800

    Added in face recognition in login page

---
+++ wwwroot/js/faceRecognition.js
@@ -0,0 +1,78 @@
+const video = document.getElementById("video");
+
+
+
+Promise.all([
+    faceapi.nets.ssdMobilenetv1.loadFromUri("../models"),
+    faceapi.nets.faceLandmark68Net.loadFromUri("../models"),
+    faceapi.nets.faceRecognitionNet.loadFromUri("../models"),
+]).then(startWebcam);
+
+
+function startWebcam() {
+    navigator.mediaDevices
+        .getUserMedia({
+            video: true,
+            audio: false,
+        })
+        .then((stream) => {
+            video.srcObject = stream;
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+}
+
+function getLabeledFaceDescriptions() {
+    const labels = ["xinyin", "keene", "xuewen"];
+    return Promise.all(
+        labels.map(async (label) => {
+            const descriptions = [];
+            for (let i = 1; i <= 1; i++) {
+                const img = await faceapi.fetchImage(`../labels/${label}.png`);
+                const detections = await faceapi
+                    .detectSingleFace(img)
+                    .withFaceLandmarks()
+                    .withFaceDescriptor();
+                descriptions.push(detections.descriptor);
+            }
+            return new faceapi.LabeledFaceDescriptors(label, descriptions);
+        })
+    );
+}
+
+video.addEventListener("play", async () => {
+    const labeledFaceDescriptors = await getLabeledFaceDescriptions();
+    const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors);
+
+    const canvas = faceapi.createCanvasFromMedia(video);
+    document.body.append(canvas);
+
+    const displaySize = { width: video.width, height: video.height };
+    faceapi.matchDimensions(canvas, displaySize);
+
+    setInterval(async () => {
+        const detections = await faceapi
+            .detectAllFaces(video)
+            .withFaceLandmarks()
+            .withFaceDescriptors();
+
+        const resizedDetections = faceapi.resizeResults(detections, displaySize);
+
+        canvas.style.position = 'fixed';
+        canvas.style.zIndex = 1;
+        canvas.style.top = '8rem';
+        canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+
+        const results = resizedDetections.map((d) => {
+            return faceMatcher.findBestMatch(d.descriptor);
+        });
+        results.forEach((result, i) => {
+            const box = resizedDetections[i].detection.box;
+            const drawBox = new faceapi.draw.DrawBox(box, {
+                label: result,
+            });
+            drawBox.draw(canvas);
+        });
+    }, 100);
+});
